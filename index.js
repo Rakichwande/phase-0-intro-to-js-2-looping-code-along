1751b3a00b1f287031eeff3d30d0cf301c51ecd2
Joshua <rakichwande@gmail.com>
2022-08-26 18:10:04 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards (name, event) {
+    let note =[]
+    for (let i = 0; i < name.length; i++) {
+    console.log (note.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`));
+    }
+    return note
+}
+
+writeCards(["Ada","Brendan","Ali"],"surprise");
+function countDown () {
+    let i = 0
+    while (i <=10) {
+        console.log(i)
+        i++
+    }
+}
+
